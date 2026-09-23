b66af38764e0cd35a59dd9123b301b1b6d6e7743
Elian <aburtoelian3@gmail.com>
2026-09-23 09:35:02 -0600

    Habilitar instalacion PWA directa en celular y corregir compilacion de APK en GitHub Actions

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'italika-estructuras-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './vacaciones.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Network first, fallback to cache
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.status === 200 && event.request.method === 'GET') {
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
